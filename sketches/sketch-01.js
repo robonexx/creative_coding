8a3135c42a2840ed309024be1a0c368dbe0160d7
robonexx <robertwagar@gmail.com>
2021-12-28 17:20:51 +0100

    added some code to first sketch, added folder to save images in png format

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,33 +1,41 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [2048, 2048],
+  dimensions: [1080, 1080],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#282828';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.005;
 
     // canvas and context 2d
 
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w = width * 0.1;
+    const h = height * 0.1;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const off = width * 0.02;
     let x, y;
 
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
 
         context.beginPath();
+        context.strokeStyle = '#f8f8f8';
         context.rect(x, y, w, h);
         context.stroke();
 
         if (Math.random() > 0.5) {
           context.beginPath();
-          context.rect(x + 8, y + 8, w - 16, h - 16);
+          context.fillStyle = '#000';
+          context.fillRect(x + off / 2, y + off / 2, w - off, h - off);
+          context.rect(x + off / 2, y + off / 2, w - off, h - off);
           context.stroke();
         }
       }
